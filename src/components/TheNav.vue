ac56cf69539516333e38ea828088aa783ac5068d
phpusr <phpusr@gmail.com>
2024-02-25 14:55:25 +0500

    Moved nav items to constants

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="sticky-bottom bg-white p-0">
     <ul class="d-flex align-items-center justify-content-around border-top m-0">
-      <NavItem v-for="navItem in navItems" :key="navItem.action"
+      <NavItem v-for="navItem in NAV_ITEMS" :key="navItem.action"
                :to="`/${navItem.action}`"
                :class="{
                  'active-nav-item' : currentPage === navItem.action,
@@ -13,25 +13,21 @@
     </ul>
   </nav>
 </template>
-<script setup lang="ts">
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
-import NavItem from "./NavItem.vue";
-import { useRoute } from "vue-router";
-import { computed } from "vue";
-import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants'
 
-const navItems = [
-  { title: 'Timeline', action: PAGE_TIMELINE, icon: ClockIcon },
-  { title: 'Activities', action: PAGE_ACTIVITIES, icon: ListBulletIcon },
-  { title: 'Progress', action: PAGE_PROGRESS, icon: ChartBarIcon }
-]
+<script setup lang="ts">
+import NavItem from './NavItem.vue'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+import { NAV_ITEMS } from '../constants'
 
 const route = useRoute()
 const currentPage = computed(() => route.path.split('/')[1])
 </script>
+
 <style scoped>
 nav ul {
   list-style: none;
+  padding-inline-start: 0;
 }
 .nav-icon {
   height: 30px;
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,12 @@
+import { ChartBarIcon, ClockIcon, ListBulletIcon } from '@heroicons/vue/24/outline/index.js'
+
 export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
 export const PAGE_DAY_COMPLETE = 'day-complete'
+export const NAV_ITEMS = [
+  { title: 'Timeline', action: PAGE_TIMELINE, icon: ClockIcon },
+  { title: 'Activities', action: PAGE_ACTIVITIES, icon: ListBulletIcon },
+  { title: 'Progress', action: PAGE_PROGRESS, icon: ChartBarIcon }
+]
+
