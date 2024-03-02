1fc7a383dfad5c16d02e5e2b5d4c2baa246ae379
phpusr <phpusr@gmail.com>
2024-03-02 17:55:27 +0500

    Added ActivityItem component

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <li class="d-flex flex-column p-3 divide-y">
+    <div class="d-flex align-items-center">
+      <BaseButton>
+        <TrashIcon />
+      </BaseButton>
+      <div class="ms-2 text-truncate fs-3">{{ activity }}</div>
+    </div>
+    <div class="mt-2">
+      <BaseSelect
+          class="font-monospace"
+          placeholder="h:mm"
+          :selected="secondsToComplete"
+          :options="periodSelectOptions"
+          @select="secondsToComplete = $event"
+      />
+    </div>
+  </li>
+</template>
+<script setup>
+import { TrashIcon } from '@heroicons/vue/24/outline/index.js'
+import BaseButton from './BaseButton.vue'
+import BaseSelect from './BaseSelect.vue'
+import { ref } from 'vue'
+
+defineProps({
+  activity: {
+    type: String,
+    required: true
+  }
+})
+
+const periodSelectOptions = [15, 30, 45].map(it => ({ label: `0:${it}`, value: it }))
+
+const secondsToComplete = ref(null)
+</script>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,28 +1,12 @@
 <template>
   <div>
     <ul class="p-0">
-      <li v-for="activity in activities" :key="activity" class="d-flex flex-column p-3 divide-y">
-        <div class="d-flex align-items-center">
-          <BaseButton>
-            <TrashIcon />
-          </BaseButton>
-          <div class="ms-2 text-truncate fs-3">{{ activity }}</div>
-        </div>
-        <div class="mt-2">
-          <BaseSelect class="font-monospace" placeholder="h:mm" :options="periodSelectOptions" />
-        </div>
-      </li>
+      <ActivityItem v-for="activity in activities" :key="activity" :activity="activity" />
     </ul>
   </div>
 </template>
 <script setup>
-import { TrashIcon } from '@heroicons/vue/24/outline/index.js'
-import BaseButton from '../components/BaseButton.vue'
-import BaseSelect from '../components/BaseSelect.vue'
+import ActivityItem from '../components/ActivityItem.vue'
 
 const activities = ['Coding', 'Reading', 'Training']
-const periodSelectOptions = [15, 30, 45].map(it => ({ label: `0:${it}`, value: it }))
 </script>
-<style scoped>
-
-</style>
