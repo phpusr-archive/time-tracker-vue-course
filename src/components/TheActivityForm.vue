ea327c0bb214ad7c42481d7e87f5b92504cc681d
phpusr <phpusr@gmail.com>
2024-03-03 19:18:09 +0500

    Added TheActivityForm component

---
+++ src/components/TheActivityForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <form class="sticky-bottom d-flex p-3" @submit.prevent="store.addActivity(activity)">
+    <input type="text" class="form-control me-2" placeholder="Activity name" v-model="activity">
+    <BaseButton>
+      <PlusIcon />
+    </BaseButton>
+  </form>
+</template>
+<script setup>
+import { ref } from 'vue'
+import { PlusIcon } from '@heroicons/vue/24/outline/index.js'
+import BaseButton from './BaseButton.vue'
+import { useAppStore } from '../stores/index.js'
+
+const store = useAppStore()
+
+const activity = ref('')
+</script>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -4,24 +4,15 @@
       <ActivityItem v-for="activity in store.activities" :key="activity" :activity="activity" />
     </ul>
 
-    <form class="sticky-bottom d-flex p-3" @submit.prevent="store.addActivity(newActivity)">
-      <input type="text" class="form-control me-2" placeholder="Activity name" v-model="newActivity">
-      <BaseButton>
-        <PlusIcon />
-      </BaseButton>
-    </form>
+    <TheActivityForm />
   </div>
 </template>
 <script setup>
-import { PlusIcon } from '@heroicons/vue/24/outline/index.js'
 import ActivityItem from '../components/ActivityItem.vue'
-import BaseButton from '../components/BaseButton.vue'
 import { useAppStore } from '../stores/index.js'
-import { ref } from 'vue'
+import TheActivityForm from '../components/TheActivityForm.vue'
 
 const store = useAppStore()
-
-const newActivity = ref('')
 </script>
 <style scoped>
 form {
