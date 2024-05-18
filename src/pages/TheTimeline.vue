b53be8247475902b51887c793cdd634a7cf41bbf
phpusr <phpusr@gmail.com>
2024-05-18 20:25:44 +0500

    Scroll page to current timeline item

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="mt-5">
     <ul class="p-0">
-      <TimelineItem v-for="timelineItem in store.timelineItems" :key="timelineItem.hour" :timelineItem="timelineItem" />
+      <TimelineItem
+          v-for="timelineItem in store.timelineItems"
+          :key="timelineItem.hour"
+          :timelineItem="timelineItem"
+          ref="timelineItemRefs"
+      />
     </ul>
   </div>
 </template>
@@ -9,6 +14,18 @@
 <script setup>
 import TimelineItem from '../components/TimelineItem.vue'
 import { useAppStore } from '../stores/index.js'
+import { onMounted, ref } from 'vue'
 
 const store = useAppStore()
+
+const timelineItemRefs = ref([])
+
+onMounted(scrollToCurrentTimelineItem)
+
+function scrollToCurrentTimelineItem() {
+  const currentHour = new Date().getHours()
+  if (currentHour > 0) {
+    timelineItemRefs.value[currentHour - 1].$el.scrollIntoView()
+  }
+}
 </script>
