94125230e5907c26885b494e51cea51ead312c76
phpusr <phpusr@gmail.com>
2024-05-19 21:34:20 +0500

    Constants refactoring

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,7 @@ export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
 export const PAGE_DAY_COMPLETE = 'day-complete'
+
 export const NAV_ITEMS = [
   { title: 'Timeline', action: PAGE_TIMELINE, icon: ClockIcon },
   { title: 'Activities', action: PAGE_ACTIVITIES, icon: ListBulletIcon },
@@ -15,6 +16,7 @@ export const MAX_HOUR = 23
 export const MINUTES_IN_HOUR = 60
 export const SECONDS_IN_MINUTE = 60
 export const SECONDS_IN_HOUR = MINUTES_IN_HOUR * SECONDS_IN_MINUTE
+
 export const PERIOD_SELECT_OPTIONS = generatePeriodSelectOptions()
 
 export const PRIMARY_BUTTON_TYPE = 'primary'
@@ -22,5 +24,7 @@ export const SUCCESS_BUTTON_TYPE = 'success'
 export const WARNING_BUTTON_TYPE = 'warning'
 export const NEUTRAL_BUTTON_TYPE = 'neutral'
 export const DANGER_BUTTON_TYPE = 'danger'
-export const BUTTON_TYPES = [PRIMARY_BUTTON_TYPE, SUCCESS_BUTTON_TYPE, WARNING_BUTTON_TYPE,
-  NEUTRAL_BUTTON_TYPE, DANGER_BUTTON_TYPE]
+
+export const BUTTON_TYPES = [
+  PRIMARY_BUTTON_TYPE, SUCCESS_BUTTON_TYPE, WARNING_BUTTON_TYPE, NEUTRAL_BUTTON_TYPE, DANGER_BUTTON_TYPE
+]
