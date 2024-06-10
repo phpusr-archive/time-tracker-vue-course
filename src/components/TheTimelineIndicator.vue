832908841469bd046a36606315964258a97aaef1
phpusr <phpusr@gmail.com>
2024-06-10 20:22:24 +0500

    Added useSecondsSinceMidnight

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -7,35 +7,16 @@
 </template>
 
 <script setup>
-import { computed, onUnmounted, ref } from 'vue'
-import { SECONDS_IN_DAY, SECONDS_IN_HOUR, SECONDS_IN_MINUTE } from '../constants'
+import { computed, ref } from 'vue'
+import { useSecondsSinceMidnight } from '../composables/seconds-since-midnight'
 
-const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
 const indicatorRef = ref()
 
-let timer = setInterval(() => {
-  secondsSinceMidnight.value++
-  if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
-    secondsSinceMidnight.value = 0
-  }
-}, 1000)
-
-onUnmounted(() => {
-  clearInterval(timer)
-})
+const { secondsSinceMidnightInPercentage } = useSecondsSinceMidnight()
 
 const calculateTopOffset = computed(() => {
-  return calculateSecondsSinceMidnightInPercentage.value / 100 * getTimelineHeight() - 18
+  return secondsSinceMidnightInPercentage.value / 100 * getTimelineHeight() - 18
 })
-
-const calculateSecondsSinceMidnightInPercentage = computed(() => {
-  return secondsSinceMidnight.value / SECONDS_IN_DAY * 100
-})
-
-function calculateSecondsSinceMidnight() {
-  const now = new Date()
-  return now.getHours() * SECONDS_IN_HOUR + now.getMinutes() * SECONDS_IN_MINUTE + now.getSeconds()
-}
 
 function getTimelineHeight() {
   return indicatorRef.value?.parentNode.getBoundingClientRect().height
