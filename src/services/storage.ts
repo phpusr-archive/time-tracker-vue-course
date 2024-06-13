5d882f7672f3679fe826c754fe91455b4fdf7d22
phpusr <phpusr@gmail.com>
2024-06-13 12:25:23 +0500

    Converted storage

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,12 +1,13 @@
 import { LOCAL_STORAGE_KEY } from '../constants'
 import { today } from './time'
+import type { State } from '../types'
 
-export function load() {
+export function load(): State {
   const serializedState = localStorage.getItem(LOCAL_STORAGE_KEY)
   return serializedState ? JSON.parse(serializedState) : {}
 }
 
-export function save(state) {
+export function save(state: State): void {
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({
     ...state,
     lastActiveAt: today().toISOString()
